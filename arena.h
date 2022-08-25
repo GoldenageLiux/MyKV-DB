ed3dd78e4f9232b2072f9075ad4cd7cfc70c72d3
GoldenageLiux <1637364952@qq.com>
2022-08-25 16:42:36 +0800

    [Add] arena.h

---
+++ arena.h
@@ -0,0 +1,29 @@
+/**
+ * @Author: liuxin
+ * @Date:   2022-08-25 14:36:20
+ * @Last Modified by:   liuxin
+ * @Last Modified time: 2022-08-25 16:34:52
+ */
+#include <stdint.h>
+
+#ifndef __ARENA_H__
+#define __ARENA_H__
+
+class Arena
+{
+private:
+    /* data */
+public:
+    Arena(/* args */);
+    ~Arena();
+};
+
+Arena::Arena(/* args */)
+{
+}
+
+Arena::~Arena()
+{
+}
+
+#endif /* __ARENA_H__ */
